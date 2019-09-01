2d537cc82ab7ab2dc0fd73215933400822f20856
flagarde <lagarde@sjtu.edu.cn>
2019-09-02 01:56:12 +0800

    Clean

--- include/X742CorrectionRoutines.hpp
+++ include/X742CorrectionRoutines.hpp
@@ -1,19 +1,3 @@
-/******************************************************************************
-*
-* CAEN SpA - Front End Division
-* Via Vetraia, 11 - 55049 - Viareggio ITALY
-* +390594388398 - www.caen.it
-*
-***************************************************************************//**
-* \note TERMS OF USE:
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the GNU General Public License as published by the Free Software
-* Foundation. This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the
-* software, documentation and results solely at his own risk.
-******************************************************************************/
-
 #ifndef __X742_CORRECTION_ROUTINES_H
 #define __X742_CORRECTION_ROUTINES_H
 
