60ef76937b09d21fa43086b04a4c81d8c2e52ff1
flagarde <lagarde@sjtu.edu.cn>
2019-09-02 16:37:27 +0800

    Supress useless definitions

--- include/WDconfig.hpp
+++ include/WDconfig.hpp
@@ -20,33 +20,6 @@
 */
 int ParseConfigFile(FILE *f_ini, WaveDumpConfig_t *WDcfg);
 
-/*! \fn      int WriteRegisterBitmask(int32_t handle, uint32_t address, uint32_t data, uint32_t mask)
-*   \brief   writes 'data' on register at 'address' using 'mask' as bitmask
-*
-*   \param   handle :   Digitizer handle
-*   \param   address:   Address of the Register to write
-*   \param   data   :   Data to Write on the Register
-*   \param   mask   :   Bitmask to use for data masking
-*   \return  0 = Success; negative numbers are error codes
-*/
-int WriteRegisterBitmask(int32_t handle, uint32_t address, uint32_t data, uint32_t mask);
-
-/*! \fn      int ProgramDigitizer(int handle, WaveDumpConfig_t WDcfg) 
-*   \brief   configure the digitizer according to the parameters read from
-*            the cofiguration file and saved in the WDcfg data structure
-*            
-*   \param   handle   Digitizer handle
-*   \param   WDcfg:   WaveDumpConfig data structure
-*   \return  0 = Success; negative numbers are error codes
-*/
-int ProgramDigitizer(int handle, WaveDumpConfig_t WDcfg, CAEN_DGTZ_BoardInfo_t BoardInfo);
-
-/*! \fn      void CheckKeyboardCommands(WaveDumpRun_t *WDrun) 
-*   \brief   check if there is a key pressed and execute the relevant command
-*            
-*   \param   WDrun:   Pointer to the WaveDumpRun data structure
-*/
-void CheckKeyboardCommands(int handle, WaveDumpRun_t *WDrun, WaveDumpConfig_t *WDcfg, CAEN_DGTZ_BoardInfo_t BoardInfo);
 
 /*! \fn      void Load_DAC_Calibration_From_Flash(int handle, WaveDumpConfig_t *WDcfg, CAEN_DGTZ_BoardInfo_t BoardInfo)
 *   \brief   look for DAC calibration in flash and load it
