c4630ce1ab5ba24646e311fa1b89ffff70351047
flagarde <lagarde@sjtu.edu.cn>
2019-12-20 18:38:26 +0800

    Only the latex osx

--- .travis-ci.d/install_dependencies.sh
+++ .travis-ci.d/install_dependencies.sh
@@ -6,10 +6,7 @@ mkdir -p dependencies && cd dependencies
 # install root
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then export ROOT_BUILD="Linux-ubuntu18-x86_64-gcc7.4";
-elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then export ROOT_BUILD="macosx64-";
-    if [[ "${TRAVIS_OSX_IMAGE}" == "xcode10.13" ]]; then export ROOT_BUILD=$ROOT_BUILD"10.13-clang100";
-    elif [[ "${TRAVIS_OSX_IMAGE}" == "xcode10.14" ]]; then export ROOT_BUILD=$ROOT_BUILD"10.14-clang100";
-    fi;
+elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then export ROOT_BUILD="macosx64-10.14-clang100";
 fi
 export ROOT_BIN="root_v${ROOT_VERSION}.${ROOT_BUILD}.tar.gz"
 wget https://root.cern.ch/download/${ROOT_BIN}
