1fb072d015f84fb5dd771703021e7432c051dedf
flagarde <lagarde@sjtu.edu.cn>
2020-01-14 21:44:02 +0800

    Clean

--- source/include/Module.hpp
+++ source/include/Module.hpp
@@ -24,68 +24,51 @@ public :
   virtual void Release();
   std::string getName();
   std::string getType();
-  virtual void OnOpen(const std::string&);
-  virtual void OnClose(const std::string&);
-  virtual void OnPong(const std::string&);
-  virtual void OnPing(const std::string&);
-  virtual void OnMessage(const std::string&);
+  virtual void OnOpen(const ix::WebSocketMessagePtr& msg);
+  virtual void OnClose(const ix::WebSocketMessagePtr& msg);
+  virtual void OnPong(const ix::WebSocketMessagePtr& msg);
+  virtual void OnPing(const ix::WebSocketMessagePtr& msg);
+  virtual void OnMessage(const ix::WebSocketMessagePtr& msg);
+  virtual void OnError(const ix::WebSocketMessagePtr& msg);
   
   ix::WebSocketSendInfo sendBinary(Message& message);
   ix::WebSocketSendInfo sendText(Message& message);
   
-  
-  
 private:
+  void DoOnMessage(const ix::WebSocketMessagePtr& msg);
   WebsocketClient m_WebsocketClient;
   std::string m_Name{"Unknown"};
   std::string m_Type{"Unknown"};
-  std::function<void(const ix::WebSocketMessagePtr&)> m_CallBack{[this](const ix::WebSocketMessagePtr& msg)
+  std::function<void(const ix::WebSocketMessagePtr&)> m_CallBack
   {
-    if (msg->type == ix::WebSocketMessageType::Message)
+    [this](const ix::WebSocketMessagePtr& msg)
     {
-      //this->OnMessage(msg->str);
-      std::cout << msg->str << std::endl;
-      std::cout << getName() << std::endl;
-    }
-    
-    else if (msg->type == ix::WebSocketMessageType::Open)
-    {
-      std::cout << "send greetings" << std::endl;
-      
-      // Headers can be inspected (pairs of string/string)
-      std::cout << "Handshake Headers:" << std::endl;
-      for (auto it : msg->openInfo.headers)
+      if (msg->type == ix::WebSocketMessageType::Message)
       {
-        std::cout << it.first << ": " << it.second << std::endl;
+        this->DoOnMessage(msg);
+      }
+      else if (msg->type == ix::WebSocketMessageType::Open)
+      {
+        this->OnOpen(msg);
+      }
+      else if (msg->type == ix::WebSocketMessageType::Close)
+      {
+        this->OnClose(msg);
+      }
+      else if (msg->type == ix::WebSocketMessageType::Error)
+      {
+        this->OnError(msg);
+      }
+      else if (msg->type == ix::WebSocketMessageType::Ping)
+      {
+        this->OnPing(msg);
+      }
+      else if(msg->type == ix::WebSocketMessageType::Pong)
+      {
+        this->OnPong(msg);
       }
     }
-    else if (msg->type == ix::WebSocketMessageType::Close)
-    {
-      std::cout << "disconnected" << std::endl;
-      
-      // The server can send an explicit code and reason for closing.
-      // This data can be accessed through the closeInfo object.
-      std::cout << msg->closeInfo.code << std::endl;
-      std::cout << msg->closeInfo.reason << std::endl;
-    }
-    else if (msg->type == ix::WebSocketMessageType::Error)
-    {
-      std::stringstream ss;
-      ss << "Error: "         << msg->errorInfo.reason      << std::endl;
-      ss << "#retries: "      << msg->errorInfo.retries     << std::endl;
-      ss << "Wait time(ms): " << msg->errorInfo.wait_time   << std::endl;
-      ss << "HTTP Status: "   << msg->errorInfo.http_status << std::endl;
-      std::cout << ss.str() << std::endl;
-    }
-    else if (msg->type == ix::WebSocketMessageType::Ping)
-    {
-      std::cout << "pong data: " << msg->str << std::endl;
-    }
-    else if(msg->type == ix::WebSocketMessageType::Pong)
-    {
-      
-    }
-  }};
+  };
 };
 
 #endif
--- source/src/Module.cpp
+++ source/src/Module.cpp
@@ -47,8 +47,55 @@ void Module::Clear(){};
 void Module::Disconnect(){};
 void Module::Release(){};
 
-void Module::OnOpen(const std::string&){};
-void Module::OnClose(const std::string&){};
-void Module::OnPong(const std::string&){};
-void Module::OnPing(const std::string&){};
-void Module::OnMessage(const std::string&){};
+void Module::DoOnMessage(const ix::WebSocketMessagePtr& msg)
+{
+  
+  std::cout << msg->str << std::endl;
+}
+
+
+
+void Module::OnOpen(const ix::WebSocketMessagePtr& msg)
+{
+  // Headers can be inspected (pairs of string/string)
+  std::cout << "Handshake Headers :" << std::endl;
+  for (auto it : msg->openInfo.headers)
+  {
+    std::cout << it.first << ": " << it.second << std::endl;
+  }
+}
+
+void Module::OnClose(const ix::WebSocketMessagePtr& msg)
+{
+  std::cout << "Disconnected !" << std::endl;
+  
+  // The server can send an explicit code and reason for closing.
+  // This data can be accessed through the closeInfo object.
+  std::cout << msg->closeInfo.code << std::endl;
+  std::cout << msg->closeInfo.reason << std::endl;
+}
+
+void Module::OnPong(const ix::WebSocketMessagePtr& msg)
+{
+  std::cout << "Pong data: " << msg->str << std::endl;
+}
+
+void Module::OnPing(const ix::WebSocketMessagePtr& msg)
+{
+  std::cout << "Ping data: " << msg->str << std::endl;
+}
+
+void Module::OnMessage(const ix::WebSocketMessagePtr& msg)
+{
+  std::cout << msg->str << std::endl;
+}
+
+void Module::OnError(const ix::WebSocketMessagePtr& msg)
+{
+  std::stringstream ss;
+  ss << "Error: "         << msg->errorInfo.reason      << std::endl;
+  ss << "#retries: "      << msg->errorInfo.retries     << std::endl;
+  ss << "Wait time(ms): " << msg->errorInfo.wait_time   << std::endl;
+  ss << "HTTP Status: "   << msg->errorInfo.http_status << std::endl;
+  std::cout << ss.str() << std::endl;
+}
