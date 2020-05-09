d4116a696d457d0adca7b49d678ff76c85174e56
flagarde <flagarde@users.noreply.github.com>
2020-05-10 00:06:48 +0800

    Update install_dependencies.sh

--- .travis-ci.d/install_dependencies.sh
+++ .travis-ci.d/install_dependencies.sh
@@ -5,7 +5,7 @@ mkdir -p dependencies && cd dependencies
 
 # install root
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then export ROOT_BUILD="Linux-ubuntu18-x86_64-gcc7.4";
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then export ROOT_BUILD="Linux-ubuntu18-x86_64-gcc7.5";
 elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then export ROOT_BUILD="macosx64-10.14-clang100";
 fi
 export ROOT_BIN="root_v${ROOT_VERSION}.${ROOT_BUILD}.tar.gz"
