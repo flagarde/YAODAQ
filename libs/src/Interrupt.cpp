0d16314fe27c8c40b64513495e7efc958babfd2c
flagarde <lagarde@sjtu.edu.cn>
2020-05-18 03:01:32 +0800

    Fix

--- libs/src/Interrupt.cpp
+++ libs/src/Interrupt.cpp
@@ -8,32 +8,32 @@ volatile bool Interrupt::m_Continue = true;
 Interrupt::Interrupt()
 {
   std::signal(SIGTERM, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::warn("Termination request, sent to the program ");
     m_Continue = false;
   });
   std::signal(SIGSEGV, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::critical("Invalid memory access (segmentation fault).");
     m_Continue = false;
   });
   std::signal(SIGINT, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::warn("Interrupt signal received.");
     m_Continue = false;
   });
   std::signal(SIGILL, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::critical("Invalid program image.");
     m_Continue = false;
   });
   std::signal(SIGABRT, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::error("Abnormal termination condition.");
     m_Continue = false;
   });
   std::signal(SIGFPE, [](int a) -> void {
-    std::cout << "\n";
+    fmt::print("\n");
     spdlog::critical("Erroneous arithmetic operation.");
     m_Continue = false;
   });
