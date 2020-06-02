79786cb03216636bda50e501f0793801789dee54
flagarde <lagarde@sjtu.edu.cn>
2020-06-02 16:52:15 +0800

    Better CAEN

--- extras/CAEN/external/CAENlibs.cmake
+++ extras/CAEN/external/CAENlibs.cmake
@@ -1,24 +1,36 @@
-include(ExternalProject)
-
-if (NOT TARGET CAENlibs)
-# ----- CAENlibs package -----
-ExternalProject_Add( 
-                    CAENlibs
-                    GIT_REPOSITORY ${CAENlibs_repository}
-                    GIT_TAG ${CAENlibs_version}
-                    GIT_PROGRESS TRUE
-                    GIT_SHALLOW TRUE
-                    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCAENALL=TRUE -DINCLUDE_OUTPUT_DIRECTORY=${CMAKE_BINARY_DIR}/CAEN
-                    PREFIX ${CMAKE_BINARY_DIR}/CAENlibs-prefix
-                    SOURCE_DIR ${CMAKE_BINARY_DIR}/CAENlibs
-                    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-                    LOG_DOWNLOAD ON
+if(NOT TARGET CAENlibs)
+  include(ExternalProject)
+  find_package(Threads)
+  set(THREADS_PREFER_PTHREAD_FLAG TRUE)
+  # ----- CAENlibs package -----
+  ExternalProject_Add( 
+                      CAENlibs
+                      GIT_REPOSITORY ${CAENlibs_repository}
+                      GIT_TAG ${CAENlibs_version}
+                      GIT_PROGRESS TRUE
+                      GIT_SHALLOW TRUE
+                      CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCAENALL=TRUE -DINCLUDE_OUTPUT_DIRECTORY=${CMAKE_BINARY_DIR}/CAEN
+                      PREFIX ${CMAKE_BINARY_DIR}/CAENlibs-prefix
+                      SOURCE_DIR ${CMAKE_BINARY_DIR}/CAENlibs
+                      INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+                      LOG_DOWNLOAD ON
                     )      
-
-if(UNIX)
-   set(CAEN_DEFS "-DUNIX" "-DLINUX" CACHE INTERNAL "CAEN definitions" FORCE)
-elseif(WIN32)
-   set(CAEN_DEFS "-D_WIN32" "-DWIN32" CACHE INTERNAL "CAEN definitions" FORCE)
-endif()
-mark_as_advanced(FORCE CAEN_DEFS)
+  add_library(CAEN INTERFACE)
+  add_dependencies(CAEN CAENlibs)
+  if(UNIX)
+    target_compile_definitions(CAEN INTERFACE "UNIX" INTERFACE "LINUX")
+  elseif(WIN32)
+    target_compile_definitions(CAEN INTERFACE "_WIN32" INTERFACE "WIN32")
+  endif()
+  target_link_libraries(CAEN
+    INTERFACE CAENVME
+    INTERFACE CAENComm
+    INTERFACE CAENDigitizer
+    INTERFACE caenhvwrapper
+    INTERFACE ${CMAKE_DL_LIBS}
+    INTERFACE Threads::Threads
+    INTERFACE $<$<OR:$<STREQUAL:${ARCHITECTURE},x64>,$<STREQUAL:${ARCHITECTURE},x86>>:CAENDPP>
+    )
+  target_include_directories(CAEN INTERFACE $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/CAEN>)
+  target_link_directories(CAEN INTERFACE ${LIBRARY_OUTPUT_DIR})
 endif()
