a987f4bf2a0b1065b08c46ac34813ce88a18c34f
flagarde <8627746+flagarde@users.noreply.github.com>
2021-05-27 14:57:08 +0800

    Move send commands to MessageHandlerClient

--- include/Module.hpp
+++ include/Module.hpp
@@ -50,21 +50,7 @@ public:
   void                                   printParameters();
   void                                   stopListening();
   void                                   startListening();
-  /*void                                   send(Message& message)
-  {
-    message.setFrom(getIdentifier().getType() + "/" + getIdentifier().getName());
-    MessageHandlerClient::send(message.get());
-  }
-  void                                   sendText(Message& message)
-  {
-    message.setFrom(getIdentifier().getType() + "/" + getIdentifier().getName());
-    MessageHandlerClient::sendText(message.get());
-  }
-  void                                   sendBinary(Message& message)
-  {
-    message.setFrom(getIdentifier().getType() + "/" + getIdentifier().getName());
-    MessageHandlerClient::sendBinary(message.get());
-  }*/
+
   template<typename... Args> inline void sendTrace(yaodaq_string_view fmt, const Args&... args)
   {
     fmt::memory_buffer buf;
