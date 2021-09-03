0dfd914c1dc03dd773edccce78a0642de9f5ceda
flagarde <flagarde.programs@pm.me>
2021-09-03 09:39:06 +0800

    Update Utilities.hpp

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -21,7 +21,7 @@ static inline std::filesystem::path getFullExePath()
   #if  defined(__APPLE__)
     std::uint32_t bufSize{PATH_MAX + 1};
     char result[bufSize]{'\0'};
-    _NSGetExecutablePath(result,&bufSize)
+    _NSGetExecutablePath(result,&bufSize);
     return std::string(result);
   #elif defined(_WIN32)
     TCHAR result[MAX_PATH]{'\0'};
